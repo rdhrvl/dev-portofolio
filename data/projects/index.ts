4fa2c9671c614234e1f5c454c38d1a01bc98fc16
Reval <ridhorevaldysaputra@gmail.com>
2026-08-12 18:19:16 +0700

    fix(projects): export valmanhwa project to index

--- data/projects/index.ts
+++ data/projects/index.ts
@@ -2,8 +2,10 @@ import { Project } from "@/types";
 import { eHotelPpkd } from "./e-hotel-ppkd";
 import { successNearMeTheme } from "./successnearme-theme";
 import { callshaperRedesign } from "./callshaper-redesign";
+import { valmanhwa } from "./valmanhwa";
 
 export const projectsData: Project[] = [
+  valmanhwa,
   eHotelPpkd,
   successNearMeTheme,
   callshaperRedesign,
